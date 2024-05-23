75d1f05c51503b67b3aa297a754c129c7d4263ef
Rod Aguilar <108448668+Rodrag0lvr@users.noreply.github.com>
2024-05-23 12:24:48 -0500

    chore: initial commit.

---
+++ src/main/java/com/farmlogitech/farmlogitechbackend/FarmLogitechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.farmlogitech.farmlogitechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmLogitechBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmLogitechBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farmlogitech/farmlogitechbackend/FarmLogitechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmlogitech.farmlogitechbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmLogitechBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
