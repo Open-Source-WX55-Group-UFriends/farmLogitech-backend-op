744d3bbeccbfbc7a9c0b92ed7bb32d2482a92618
Franco <U202219284@upc.edu.pe>
2024-06-01 02:41:06 -0500

    feat(feature/task): Added attributes in income.

--- src/main/java/com/farmlogitech/farmlogitechbackend/dashboard_analitycs/domain/model/aggregates/income.java
+++ src/main/java/com/farmlogitech/farmlogitechbackend/dashboard_analitycs/domain/model/aggregates/income.java
@@ -1,4 +1,9 @@
 package com.farmlogitech.farmlogitechbackend.dashboard_analitycs.domain.model.aggregates;
 
 public class income {
+    String category;
+    String description;
+    double amount;
+    String date;
+    String Periodo;
 }
