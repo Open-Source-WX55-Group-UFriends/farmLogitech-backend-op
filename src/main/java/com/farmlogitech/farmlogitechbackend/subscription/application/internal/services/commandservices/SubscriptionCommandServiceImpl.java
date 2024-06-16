b8bc84189ffbd508f58f2bbdb713f7d56d913992
Rodrag0lvr <crodrigo7132@gmail.com>
2024-06-16 08:31:48 -0500

    feat(feature/subscription): Update method post

--- src/main/java/com/farmlogitech/farmlogitechbackend/subscription/application/internal/services/commandservices/SubscriptionCommandServiceImpl.java
+++ src/main/java/com/farmlogitech/farmlogitechbackend/subscription/application/internal/services/commandservices/SubscriptionCommandServiceImpl.java
@@ -1,11 +1,16 @@
 package com.farmlogitech.farmlogitechbackend.subscription.application.internal.services.commandservices;
+import com.farmlogitech.farmlogitechbackend.iam.infrastructure.authorization.sfs.model.UserDetailsImpl;
 import com.farmlogitech.farmlogitechbackend.subscription.domain.model.aggregates.Subscription;
 import com.farmlogitech.farmlogitechbackend.subscription.domain.model.commands.CreateSubscriptionCommand;
 import com.farmlogitech.farmlogitechbackend.subscription.domain.model.commands.UpdateSubscriptionCommand;
 import com.farmlogitech.farmlogitechbackend.subscription.domain.services.SubscriptionCommandService;
 import com.farmlogitech.farmlogitechbackend.subscription.infrastructure.persistence.jpa.SubscriptionRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.Optional;
 
 @Service
@@ -21,10 +26,22 @@ public class SubscriptionCommandServiceImpl implements SubscriptionCommandServic
 
     @Override
     public Optional<Subscription> handle(CreateSubscriptionCommand command) {
-        var subscription = new Subscription(command.price(),command.description(),command.paid(), command.profileId());
-        subscriptionRepository.save(subscription);
-        return Optional.of(subscription);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
 
+        // Verificar si el usuario tiene el rol ROLE_FARMER o ROLE_OWNER
+        boolean hasRequiredRole = authorities.stream()
+                .anyMatch(a -> a.getAuthority().equals("ROLE_FARMER"));
+
+        if (hasRequiredRole) {
+            var subscription = new Subscription(command.price(), command.description(), command.paid(), userDetails.getId());
+            subscription.setPaid(true);
+            subscriptionRepository.save(subscription);
+            return Optional.of(subscription);
+        } else {
+            throw new SecurityException("No estás autorizado para crear una suscripción");
+        }
     }
 
     @Override
