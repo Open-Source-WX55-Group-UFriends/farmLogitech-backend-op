867b897aaedcafd7c34b47d371c6db8b376fd9cd
Kuni_2005 <math.kuni123@gmail.com>
2024-06-19 13:05:44 -0500

    feat: fix authentication message

--- src/main/java/com/farmlogitech/farmlogitechbackend/monitoring/application/internal/services/commandservices/MessageCommandServiceImpl.java
+++ src/main/java/com/farmlogitech/farmlogitechbackend/monitoring/application/internal/services/commandservices/MessageCommandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.farmlogitech.farmlogitechbackend.monitoring.application.internal.services.commandservices;
 
+import com.farmlogitech.farmlogitechbackend.iam.application.internal.outboundservices.acl.ExternalFarmService;
 import com.farmlogitech.farmlogitechbackend.iam.infrastructure.authorization.sfs.model.UserDetailsImpl;
 import com.farmlogitech.farmlogitechbackend.iam.infrastructure.persistence.jpa.repositories.UserRepository;
 import com.farmlogitech.farmlogitechbackend.monitoring.domain.model.aggregates.Message;
@@ -21,12 +22,14 @@ public class MessageCommandServiceImpl implements MessageCommandService {
     private final MessageRepository messageRepository;
     private final EmployeeRepository employeeRepository;
     private final UserRepository userRepository;
+    private final ExternalFarmService externalFarmService;
 
-    public MessageCommandServiceImpl(MessageRepository messageRepository, EmployeeRepository employeeRepository, UserRepository userRepository)
+    public MessageCommandServiceImpl(MessageRepository messageRepository, EmployeeRepository employeeRepository, UserRepository userRepository, ExternalFarmService externalFarmService)
     {
         this.messageRepository = messageRepository;
         this.employeeRepository = employeeRepository;
         this.userRepository = userRepository;
+        this.externalFarmService = externalFarmService;
     }
 
 
@@ -41,20 +44,37 @@ public class MessageCommandServiceImpl implements MessageCommandService {
 
         if (isFarmer) {
             var employee = employeeRepository.findById(command.collaboratorId());
-            var userEmployeeId= userRepository.findByUsername(employee.get().getUsername());
 
-            var newMessage = new Message(command.description(), userEmployeeId.get().getId(), userDetails.getId(), userDetails.getId() );
+            if(employee.get().getFarmId() == externalFarmService.fetchFarmIdByUserId(userDetails.getId())){
 
-            var createNewMessage = messageRepository.save(newMessage);
-            createNewMessage.setTransmitterId(userDetails.getId());
-            return Optional.of(createNewMessage);
+                var userEmployeeId= userRepository.findByUsername(employee.get().getUsername());
 
+                var newMessage = new Message(command.description(), userEmployeeId.get().getId(), userDetails.getId(), userDetails.getId() );
+
+                var createNewMessage = messageRepository.save(newMessage);
+                createNewMessage.setTransmitterId(userDetails.getId());
+                return Optional.of(createNewMessage);
+            }
+
+            throw new SecurityException("You can only send messages to your workers");
 
         } else if (isWorker) {
-            var newMessage = new Message(command.description(), userDetails.getId(), command.farmerId(), userDetails.getId() );
-            var createNewMessage = messageRepository.save(newMessage);
-            createNewMessage.setTransmitterId(userDetails.getId());
-            return Optional.of(createNewMessage);
+
+            var farmer = userRepository.findById(command.farmerId());
+
+
+
+            if(externalFarmService.fetchFarmIdByUserId(farmer.get().getId()) == employeeRepository.findFarmIdByUsername(userDetails.getUsername()).get()){
+
+                var newMessage = new Message(command.description(), userDetails.getId(), farmer.get().getId(), userDetails.getId() );
+
+                var createNewMessage = messageRepository.save(newMessage);
+                createNewMessage.setTransmitterId(userDetails.getId());
+                return Optional.of(createNewMessage);
+            }
+
+            throw new SecurityException("You can only send messages to your workers");
+
         } else {
             throw new SecurityException("Only farmers or workers can create a message");
         }
